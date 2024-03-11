9ba012c0944faa261c044333abdce16740c03ee6
DeaMaduS <deamadus@gmail.com>
2024-03-11 17:18:28 -0300

    Subindo o projeto

---
+++ images-generative-ai/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenAI_API;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var api = new OpenAIAPI("ENTER THE KEY HERE");
+
+        var request = new OpenAI_API.Images.ImageGenerationRequest
+        {
+            Prompt = "A cut kitten in a basket"
+        };
+
+        var response = await api.ImageGenerations.CreateImageAsync(request);
+        Console.WriteLine("Image URL: {0}", response.Data[0].Url);
+    }
+}
